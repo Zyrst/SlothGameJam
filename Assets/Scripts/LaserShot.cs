ca3052bcdefb44f97ad51081b60df6de4e410ae3
unknown <mikryding@gmail.com>
2015-09-12 11:42:52 +0200

    laserprylar

--- Assets/Scripts/LaserShot.cs
+++ Assets/Scripts/LaserShot.cs
@@ -1,15 +1,23 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class LaserShot : MonoBehaviour {
+public class LaserShot : MonoBehaviour
+{
+    public GameObject followObject;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (followObject != null)
+        {
+            GetComponent<Transform>().position = followObject.GetComponent<Transform>().position;
+        }
+
+    }
 }
