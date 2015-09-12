1ecc1cf9f3a470b7a6863d231e92a50c3dfc095c
Jeflux <jespermathiason@gmail.com>
2015-09-12 11:09:15 +0200

    LaunchPad

---
+++ Assets/Scripts/LaunchpadScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LaunchpadScript : MonoBehaviour {
+
+    public float _force = 500f;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+    
+    void OnCollisionEnter(Collision col) {
+        GameObject g = col.gameObject;
+        Rigidbody r = g.GetComponent<Rigidbody>();
+
+        if (r != null) {
+            ContactPoint contact = col.contacts[0];
+            r.AddForceAtPosition(-contact.normal * _force, transform.position);
+        }
+
+    }
+}
