248144dc42d2d9159ba9b28170956bc561f35993
unknown <mikryding@gmail.com>
2015-09-12 15:50:44 +0200

    PowerUpsMaddafakkas

---
+++ Assets/PowerUpSpeed.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class PowerUpSpeed : PowerParent  {
+
+    public override void Activate()
+    {
+        _player.GetComponent<Rigidbody>().velocity += new Vector3(0,0,30000f);
+        base.Activate();
+    }
+
+}
