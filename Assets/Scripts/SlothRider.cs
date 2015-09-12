3ac5e4e7f1073b626948b6c3badea2fd1b671db8
Zyrst <zajreg@gmail.com>
2015-09-12 15:39:32 +0200

    Y u sne

--- Assets/Scripts/SlothRider.cs
+++ Assets/Scripts/SlothRider.cs
@@ -11,11 +11,13 @@ public class SlothRider : MonoBehaviour {
     [HideInInspector]
     public bool _addedForce = false;
     Vector3 _lastPos;
+    public Vector3 _startPos;
     enum dir { left, right };
     dir impulse = dir.left;
 
     public float travelDist = 10f;
     public float distToGround;
+    float xPos;
 
 
 	// Use this for initialization
@@ -23,63 +25,74 @@ public class SlothRider : MonoBehaviour {
 	    _body = GetComponent<Rigidbody>();
         distToGround = GetComponent<Collider>().bounds.extents.y;
         _lastPos = Vector3.zero;
+        _startPos = transform.position;
 	}
 	
 	// Update is called once per frame
 	void Update () {
         if(_body.velocity.z <= _maxSpeed)
             _body.AddForce(new Vector3(0, 0, _speed * Time.deltaTime), ForceMode.Acceleration);
-           
+        if (!_addedForce && _body.velocity.x > 0f)
+        {
+            Debug.Log("Set velocity");
+            _body.velocity.Set(0, _body.velocity.y, _body.velocity.z);
+        }
         if (_addedForce)
         {
             switch (impulse)
             {
                 case dir.left:
-                    if (transform.position.x < (_lastPos.x - travelDist))
+                    if (transform.position.x <= (_lastPos.x - travelDist))
                     {
                         float z = _body.velocity.z;
                         _body.velocity = new Vector3(0, 0, z);
                         _addedForce = false;
+                       
+                       //transform.position = new Vector3(_lastPos.x - travelDist, transform.position.y, transform.position.z);
                     }
                     break;
                 case dir.right:
-                    if (transform.position.x > (_lastPos.x + travelDist))
+                    if (transform.position.x >= (_lastPos.x + travelDist))
                     {
                         float z = _body.velocity.z;
                         _body.velocity = new Vector3(0, 0, z);
                         _addedForce = false;
+                       
+                      // transform.position = new Vector3(_lastPos.x + travelDist, transform.position.y, transform.position.z);
                     }
                     break;
             }
             
         }
+      
 
-
-        if (Input.GetKeyDown(KeyCode.A) && transform.position.x >= -1f && _body.velocity.x == 0)
+        if (Input.GetKeyDown(KeyCode.A) && transform.position.x > (_startPos.x - travelDist) && !_addedForce)
         {
             _body.AddForce(new Vector3(-10f, 0, 0), ForceMode.Impulse);
             _addedForce = true;
             _lastPos = transform.position;
             impulse = dir.left;
         }
-        if (Input.GetKeyDown(KeyCode.D) && transform.position.x <= 1f && _body.velocity.x == 0)
+        if (Input.GetKeyDown(KeyCode.D) && transform.position.x < (_startPos.x + travelDist) && !_addedForce)
         {
             _body.AddForce(new Vector3(10f, 0, 0), ForceMode.Impulse);
             _addedForce = true;
             _lastPos = transform.position;
             impulse = dir.right;
+            
         }
 
         if(Input.GetKeyDown(KeyCode.W))
         {
             Debug.Log("Fire the lazer");
-            new LaserShot();
         }
 
         if(Input.GetKeyDown(KeyCode.Space) && OnGround())
         {
             _body.AddForce(new Vector3(0, 5f, 0), ForceMode.Impulse);
         }
+
+        
 	}
 
     public bool OnGround()
@@ -99,4 +112,6 @@ public class SlothRider : MonoBehaviour {
             return false;
             
     }
+
+   
 }
