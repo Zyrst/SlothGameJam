4b8b2edb7a3a1a95643b8def9073fe5552d13548
Jeflux <jespermathiason@gmail.com>
2015-09-12 16:32:34 +0200

    LAunch fixk

--- Assets/Scripts/LaunchpadScript.cs
+++ Assets/Scripts/LaunchpadScript.cs
@@ -3,11 +3,12 @@ using System.Collections;
 
 public class LaunchpadScript : MonoBehaviour {
 
+    public Vector3 _launchDir = new Vector3(0, 1, 0);
     public float _force = 500f;
 
 	// Use this for initialization
 	void Start () {
-	}
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -21,5 +22,19 @@ public class LaunchpadScript : MonoBehaviour {
             r.AddForceAtPosition(-contact.normal * _force, transform.position);
         }
 
+    }
+
+    void OnTriggerEnter(Collider col) {
+        Debug.Log(col);
+        Rigidbody r = col.gameObject.GetComponent<Rigidbody>();
+
+        if (r != null) {
+            r.AddForceAtPosition(_launchDir.normalized * _force, transform.position);
+        }
+    }
+
+    void OnDrawGizmos() {
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(transform.position, (transform.position + _launchDir.normalized * 2));
     }
 }
